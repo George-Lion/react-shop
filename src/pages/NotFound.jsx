fbeb32372c1455b8e0eb4cec0fa1943ebb34761d
Jorge Leon <jorgeleonpicture@gmail.com>
2022-07-15 23:21:38 +0200

    Estructura Layout

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+	<div>
+		<img src='https://img.freepik.com/vector-gratis/ilustracion-concepto-uy-error-404-robot-roto_114360-1932.jpg?w=2000'/>
+	</div>
+	);
+}
+
+export default NotFound;
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
+		publicPath: '/'
 	},
 	mode: 'development',
 	resolve: {
@@ -30,7 +31,7 @@ module.exports = {
 				]
 			},
 			{
-				test: /\.s[ac]ss$/i,
+				test: /\.(css|scss)$/i,
 				use: [
 					"style-loader",
 					"css-loader",
@@ -48,4 +49,10 @@ module.exports = {
 			filename: '[name].css'
 		}),
 	],
+	devServer:{
+		historyApiFallback: true,
+        static: path.join(__dirname, 'dist'),
+        compress:true,
+        port:3005,
+    }
 }
